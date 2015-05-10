e9fc8df441c139cd7b997ab3364c9f1c0751740f
Saravanan Balaraman <balaraman.saravanan@gmail.com>
2015-05-09 19:53:00 -0500

    Exploratory Data Analysis Course Project 1

--- plot1.R
+++ plot1.R
@@ -1,16 +1,15 @@
 plot1 <- function() {
 
-## Read input file
+## Read input file, subset for dates "1/2/2007", "2/2/2007" and omit NAs
 	df <- read.table("household_power_consumption.txt",sep=';',na.strings='?',header=TRUE, nrows = 2075259)
 	df1 <- subset(df,(Date == "1/2/2007" | Date == "2/2/2007"))
 	df1 <- na.omit(df1)
 	rm(df)
 
-## Draw the plot to a PNG file
+## Draw the histogram for Global Active Power to a PNG file
 	png("plot1.png", width = 480, height = 480)
-	hist(df1$Global_active_power,col="red",xlab="Global Active Power (kilowatts)", main="Global Active Power", xaxt="n", yaxt="n")
-	axis(1, at=2*0:6)
-        axis(2, at=200*0:1200)
+	hist(df1$Global_active_power,col="red",xlab="Global Active Power (kilowatts)", main="Global Active Power")
 	dev.off()
 	
+	rm(df1)
 }
